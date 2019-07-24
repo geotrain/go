5488a572b2386f6efbac1f1cb5725b1149327155
Greg Westmoreland <gwestmor@us.ibm.com>
2019-07-24 16:02:08 -0500

    Add Values.go file

---
+++ values.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Example of putting 2 strings together
+	fmt.Println("This Is Written In " + "Go Language.")
+
+	// Example of String and addition calculation
+	fmt.Println("1+1 =", 1+1)
+
+	// Example of String and division calculation
+	fmt.Println("7.0 / 3.0 =", 7.0/3.0)
+
+	// Example of True and False Comparison
+	fmt.Println(true && false) // Will Equal To False
+	fmt.Println(true || false) // Will Equal To True
+	fmt.Println(!true)         // Will Equal To False
+
+}
