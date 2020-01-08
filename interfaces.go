fac8ecad268705956723b9423831d8cd309ab61d
Greg Westmoreland <gwestmor@us.ibm.com>
2020-01-08 15:04:36 -0600

    interfaces example

---
+++ interfaces.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Square is a square
+type Square struct {
+	Length float64
+}
+
+// Area returns the area of a square
+func (s *Square) Area() float64 {
+	return s.Length * s.Length
+}
+
+// Circle is a circle
+type Circle struct {
+	Radius float64
+}
+
+// Area returns the circle of the square
+func (c *Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// sumAreas return the sum of all the areas in the slice
+func sumAreas(shapes []Shape) float64 {
+	total := 0.0
+
+	for _, shape := range shapes {
+		total += shape.Area()
+	}
+	return total
+}
+
+// Shpae is a shape interface
+type Shape interface {
+	Area() float64
+}
+
+func main() {
+	s := &Square{20}
+	fmt.Println(s.Area())
+
+	c := &Circle{10}
+	fmt.Println(c.Area())
+
+	shapes := []Shape{s, c}
+	sa := sumAreas(shapes)
+	fmt.Println(sa)
+}
