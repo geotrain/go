95f3a110c6324d395c6316ebb4bd7a2991a35858
Greg Westmoreland <gwestmor@us.ibm.com>
2020-01-17 16:12:16 -0600

    Text to MP3 Code

---
+++ convertStringToMp3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Get string from user
+	fmt.Print("Enter text to convert to MP3: \n")
+	var input string
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	fmt.Println("You entered the following string: ", input)
+}
